896cde87ba224f32ace2b9bee022fb7405bd7843
unknown <dgarita@mail.com>
2021-07-13 11:47:07 -0600

    Interfaz de Login creada y funcional

--- src/yFrame/Login.java
+++ src/yFrame/Login.java
@@ -17,12 +17,12 @@ public class Login extends javax.swing.JFrame {
     /**
      * Creates new form Login
      */
-    Slide slait;
+    Slide slide;
     public Login() {
         initComponents();
         this.setLocationRelativeTo(null);
         
-        slait = new Slide();
+        slide = new Slide();
     }
 
     /**
@@ -34,36 +34,86 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        pnlLogin = new javax.swing.JPanel();
+        lblUsuario = new javax.swing.JLabel();
+        txtUsuario = new javax.swing.JTextField();
+        lblPassword = new javax.swing.JLabel();
+        txtPassword = new javax.swing.JPasswordField();
+        btbIngresar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
         pnlBackground = new javax.swing.JPanel();
-        lblDisplay = new javax.swing.JLabel();
-        lblExit = new javax.swing.JLabel();
         lblPublicidad2 = new javax.swing.JLabel();
         lblPublicidad1 = new javax.swing.JLabel();
         lblSlide = new javax.swing.JLabel();
         lblSlide1 = new javax.swing.JLabel();
         btnNext = new javax.swing.JButton();
         btnBack = new javax.swing.JButton();
-        pnlLogin = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        lblDisplay = new javax.swing.JLabel();
+        lblExit = new javax.swing.JLabel();
         lblFondo = new javax.swing.JLabel();
 
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(1000, 500));
+        setUndecorated(true);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        pnlLogin.setOpaque(false);
+        pnlLogin.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblUsuario.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblUsuario.setForeground(new java.awt.Color(111, 174, 2));
+        lblUsuario.setText("Usuario:");
+        pnlLogin.add(lblUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 10, 70, 20));
+
+        txtUsuario.setBackground(new java.awt.Color(33, 33, 33));
+        txtUsuario.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        txtUsuario.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(111, 174, 2)));
+        pnlLogin.add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 40, 190, -1));
+
+        lblPassword.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblPassword.setForeground(new java.awt.Color(111, 174, 2));
+        lblPassword.setText("Contraseña:");
+        pnlLogin.add(lblPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, 110, 20));
+
+        txtPassword.setBackground(new java.awt.Color(26, 26, 26));
+        txtPassword.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        txtPassword.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(111, 174, 2)));
+        pnlLogin.add(txtPassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 140, 190, -1));
+
+        btbIngresar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/GO_LOG_OFF.png"))); // NOI18N
+        btbIngresar.setBorder(null);
+        btbIngresar.setBorderPainted(false);
+        btbIngresar.setContentAreaFilled(false);
+        btbIngresar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btbIngresar.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/GO_LOG_ON.png"))); // NOI18N
+        btbIngresar.setRolloverSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/GO_LOG_ON.png"))); // NOI18N
+        btbIngresar.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/GO_LOG_ON.png"))); // NOI18N
+        btbIngresar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btbIngresarMouseClicked(evt);
+            }
+        });
+        pnlLogin.add(btbIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 190, 110, 40));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/LOGIN_INGRESO.png"))); // NOI18N
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel1MouseClicked(evt);
+            }
+        });
+        pnlLogin.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 250, 270));
+
+        getContentPane().add(pnlLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(396, -240, 250, 270));
+
+        pnlBackground.setMinimumSize(new java.awt.Dimension(1000, 500));
+        pnlBackground.setOpaque(false);
+        pnlBackground.setPreferredSize(new java.awt.Dimension(1000, 500));
         pnlBackground.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        lblDisplay.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/icons8_Expand_Arrow_32px.png"))); // NOI18N
-        lblDisplay.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                lblDisplayMouseClicked(evt);
-            }
-        });
-        pnlBackground.add(lblDisplay, new org.netbeans.lib.awtextra.AbsoluteConstraints(910, 0, 30, 70));
-
-        lblExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/icons8_Delete_32px.png"))); // NOI18N
-        lblExit.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                lblExitMouseClicked(evt);
-            }
-        });
-        pnlBackground.add(lblExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(960, 0, -1, 70));
 
         lblPublicidad2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/publi2.png"))); // NOI18N
         pnlBackground.add(lblPublicidad2, new org.netbeans.lib.awtextra.AbsoluteConstraints(1000, 100, 1000, 320));
@@ -104,22 +154,23 @@ public class Login extends javax.swing.JFrame {
         });
         pnlBackground.add(btnBack, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 440, 50, 30));
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setUndecorated(true);
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowOpened(java.awt.event.WindowEvent evt) {
-                formWindowOpened(evt);
+        getContentPane().add(pnlBackground, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+
+        lblDisplay.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/icons8_Expand_Arrow_32px.png"))); // NOI18N
+        lblDisplay.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblDisplayMouseClicked(evt);
             }
         });
-        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        getContentPane().add(lblDisplay, new org.netbeans.lib.awtextra.AbsoluteConstraints(910, 0, 30, 70));
 
-        pnlLogin.setOpaque(false);
-        pnlLogin.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/LOGIN_INGRESO.png"))); // NOI18N
-        pnlLogin.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 250, 270));
-
-        getContentPane().add(pnlLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 0, 250, 270));
+        lblExit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/icons8_Delete_32px.png"))); // NOI18N
+        lblExit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblExitMouseClicked(evt);
+            }
+        });
+        getContentPane().add(lblExit, new org.netbeans.lib.awtextra.AbsoluteConstraints(960, 0, -1, 70));
 
         lblFondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/Login.png"))); // NOI18N
         getContentPane().add(lblFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, 500));
@@ -176,6 +227,23 @@ public class Login extends javax.swing.JFrame {
         this.setState(yFrame.Login.ICONIFIED);
     }//GEN-LAST:event_lblDisplayMouseClicked
 
+    private void jLabel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel1MouseClicked
+        // TODO add your handling code here:
+        slide.jPanelYAbajo(-240, 0, 25, 5, pnlLogin);
+        slide.jPanelYArriba(0, -240, 25, 5, pnlLogin);
+    }//GEN-LAST:event_jLabel1MouseClicked
+
+    private void btbIngresarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btbIngresarMouseClicked
+        // TODO add your handling code here:
+        if (txtUsuario.getText() == "" && txtPassword.getText()=="")  {
+            JOptionPane.showMessageDialog(null, "Usuario Invalido");
+        }else{
+            Inicio inicio = new Inicio();
+            dispose();
+            inicio.setVisible(true);
+        }
+    }//GEN-LAST:event_btbIngresarMouseClicked
+
     /**
      * @param args the command line arguments
      */
@@ -212,17 +280,22 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btbIngresar;
     private javax.swing.JButton btnBack;
     private javax.swing.JButton btnNext;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel lblDisplay;
     private javax.swing.JLabel lblExit;
     private javax.swing.JLabel lblFondo;
+    private javax.swing.JLabel lblPassword;
     private javax.swing.JLabel lblPublicidad1;
     private javax.swing.JLabel lblPublicidad2;
     private javax.swing.JLabel lblSlide;
     private javax.swing.JLabel lblSlide1;
+    private javax.swing.JLabel lblUsuario;
     private javax.swing.JPanel pnlBackground;
     private javax.swing.JPanel pnlLogin;
+    private javax.swing.JPasswordField txtPassword;
+    private javax.swing.JTextField txtUsuario;
     // End of variables declaration//GEN-END:variables
 }
